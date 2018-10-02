7cad62c91ec6d77275f7968768bd50f293c8b88e
Alexandre SHENOUDA <alexandre.shenouda@devinci.fr>
2018-10-02 16:37:19 +0200

    First commit

---
+++ src/main/java/shenzou/esilv/App.java
@@ -0,0 +1,8 @@
+package shenzou.esilv;
+
+public class App {
+    public static void main( String[] args ) {
+        String htmlText = "<h1>Hello World!</h1>";
+        System.out.println( htmlText );
+    }
+}
